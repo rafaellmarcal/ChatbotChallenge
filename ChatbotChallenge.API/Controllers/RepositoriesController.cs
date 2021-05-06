773e3f2386d3ab1997759414c718d5f19105d8c9
Rafael Lima Marçal <rafaell.lima92@gmail.com>
2021-05-05 21:47:24 -0400

    Implementação da api para retorno das informações dos repositorios.

---
+++ ChatbotChallenge.API/Controllers/RepositoriesController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ChatbotChallenge.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RepositoriesController : ControllerBase
+    {
+        public RepositoriesController()
+        {
+
+        }
+
+        public IActionResult Get()
+        {
+            return Ok("It's works!");
+        }
+    }
+}
